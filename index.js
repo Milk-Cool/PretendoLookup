b38cad342a77a5447894bc0fb77272658849e331
Milk-Cool <milan-pl@mail.ru>
2024-07-20 14:10:18 +0300

    combine exports

---
+++ index.js
@@ -0,0 +1,8 @@
+export * from "./log.js";
+export * from "./storage.js";
+
+export * from "./parsers/data.js";
+export * from "./parsers/url.js";
+
+export * from "./parsers/communities.js";
+export * from "./parsers/posts.js";
